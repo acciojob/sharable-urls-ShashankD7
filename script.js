77f09ad276d72caa56eaa8bcd5d5acc019fbd863
Shashank Deshpande <128305551+ShashankD7@users.noreply.github.com>
2023-06-10 17:22:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// your code here
+let aIn = document.getElementById("name");
+let bIn = document.getElementById("year");
+
+function changeH() {
+	let h = document.getElementById("url");
+	let str = aIn.value;
+	let yr = bIn.value;
+	h.innerText = "https://localhost:8080/?name=" + str + "&year=" + yr;
+}
